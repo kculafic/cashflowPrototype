f171630994c95b6f32a69a56b73761d12dc8a692
kculafic <kculafic@gmail.com>
2018-08-14 13:46:20 -0500

    Add accounting.js, money formating

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let clicked = document.getElementById("runner");
 
 let cashFlowArray = [];
-
 let year = 2018;
 let retirementAge = 62;
 let lifeExpectancy = 90;
@@ -12,71 +11,74 @@ function run() {
   console.log('clicked');
   projectionMaker(1000000, 100000, 30000, .0375, 55, 50000);
   renderTable();
-
 }
 
 function projectionMaker(initialSavings, salary, yearlySavings, rate, age, desiredRetirementPension) {
   let timeLeftAlive = lifeExpectancy - age;
 
-    for (let i=0; i<=timeLeftAlive; i++) {
+  for (let i=0; i<=timeLeftAlive; i++) {
     let obj = new Object();
 
-      if (age >= retirementAge) {
-        obj.retiredStatus = true;
-      } else {
-        obj.retiredStatus = false;
+    if (age >= retirementAge) {
+      obj.retiredStatus = true;
+    }
+    else {
+      obj.retiredStatus = false;
+    }
+
+    obj.age = age;
+    age++;
+    obj.year = year++;
+
+    if (i === 0) {
+      let begBal = initialSavings;
+      let interest = (begBal * rate)
+      let acc = begBal + interest + yearlySavings;
+      // let dec = acc - desiredRetirementPension;
+      // dec = Math.round(dec * 100) / 100;
+
+      obj.begBalance = initialSavings;
+      obj.salary = salary;
+      obj.interest = interest;
+      obj.yearlySavings = yearlySavings;
+
+      if (obj.retiredStatus) {
+        obj.desiredRetirementPension = desiredRetirementPension;
+      }
+      else {
+        obj.desiredRetirementPension = 0;
       }
 
-      obj.age = age;
-      age++;
-      obj.year = year++;
+      obj.endBal = acc;
 
-      if (i === 0) {
-        let begBal = initialSavings;
-        let interest = (begBal * rate)
-        let acc = begBal + interest + yearlySavings;
-        // let dec = acc - desiredRetirementPension;
-        // dec = Math.round(dec * 100) / 100;
+    }
+    else if (obj.retiredStatus === true) {
+      let pensionInflation;
+      let pensionAdjustedForInflation;
 
-        obj.begBalance = initialSavings;
-        obj.salary = salary;
-        obj.interest = interest;
-        obj.yearlySavings = yearlySavings;
+      if (cashFlowArray[i-1].desiredRetirementPension > 0) {
+        pensionInflation = (cashFlowArray[i-1].desiredRetirementPension * 0.03)
+        pensionAdjustedForInflation = pensionInflation + cashFlowArray[i-1].desiredRetirementPension;
+      }
+      else {
+        pensionAdjustedForInflation = desiredRetirementPension;
+      }
 
-        if (obj.retiredStatus) {
-          obj.desiredRetirementPension = desiredRetirementPension;
-        } else {
-          obj.desiredRetirementPension = 0;
-        }
+      let begBal = cashFlowArray[i-1].endBal;
+      let interest = (begBal * rate)
+      let acc = begBal + interest;
+      let dec = acc - desiredRetirementPension;
+      dec = Math.round(dec * 100) / 100;
 
-        obj.endBal = acc;
+      obj.begBalance = dec;
+      obj.salary = 0;
+      obj.interest = Math.round(interest * 100) / 100;
+      obj.yearlySavings = 0;
+      obj.desiredRetirementPension = Math.round(pensionAdjustedForInflation * 100) / 100;
+      obj.endBal = dec;
 
-      } else if (obj.retiredStatus === true) {
-
-        let pensionInflation;
-        let pensionAdjustedForInflation;
-
-        if (cashFlowArray[i-1].desiredRetirementPension > 0) {
-          pensionInflation = (cashFlowArray[i-1].desiredRetirementPension * 0.03)
-          pensionAdjustedForInflation = pensionInflation + cashFlowArray[i-1].desiredRetirementPension;
-        } else {
-          pensionAdjustedForInflation = desiredRetirementPension;
-        }
-
-        let begBal = cashFlowArray[i-1].endBal;
-        let interest = (begBal * rate)
-        let acc = begBal + interest;
-        let dec = acc - desiredRetirementPension;
-        dec = Math.round(dec * 100) / 100;
-
-        obj.begBalance = dec;
-        obj.salary = 0;
-        obj.interest = Math.round(interest * 100) / 100;
-        obj.yearlySavings = 0;
-        obj.desiredRetirementPension = Math.round(pensionAdjustedForInflation * 100) / 100;
-        obj.endBal = dec;
-
-      } else if (obj.retiredStatus === false) {
+      }
+      else if (obj.retiredStatus === false) {
         let salaryInterest = (cashFlowArray[i-1].salary * 0.03)
         let salaryAdjustedForInflation = salaryInterest + cashFlowArray[i-1].salary;
 
@@ -104,7 +106,13 @@ function renderTable() {
 
     for (var key in cashFlowArray[i]) {
       var cell = document.createElement("td");
-      cell.innerText = cashFlowArray[i][key];
+
+      if (key === 'begBalance' || key === 'salary' || key === 'interest' || key === 'yearlySavings' || key === 'desiredRetirementPension' || key === 'endBal') {
+        cell.innerText = accounting.formatMoney(cashFlowArray[i][key]);
+      }
+      else {
+        cell.innerText = cashFlowArray[i][key];
+      }
       var newRowHolder = newRow.insertCell();
       newRowHolder.appendChild(cell);
     }
